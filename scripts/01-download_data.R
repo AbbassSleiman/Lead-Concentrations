c28e22009abc9d604837e806ebc9dc28b217946b
AbbassSleiman <abbass.sleiman2003@gmail.com>
2024-01-18 23:24:57 -0500

    Downloaded raw data
    Here I have downloaded and saved the raw data for the lead concentration study into the data folder under inputs.

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,26 +1,34 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Here we download the data for the study directly from opendatatoronto,
+# in particular the downloaded data is "Non Regulated Lead Sample".
+# Author: Abbass Sleiman
+# Date: 18 January 2024
+# Contact: abbass.sleiman@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: None required
 
 
 #### Workspace setup ####
 library(opendatatoronto)
 library(tidyverse)
+library(janitor)
 # [...UPDATE THIS...]
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
+raw_lead_data <- 
+  read_csv(
+  file =
+    "https://ckan0.cf.opendata.inter.prod-toronto.ca/dataset/8171f7ab-364e-46a7-8a62-08f073ca6d96/resource/4b141457-e2a5-43e9-b5a4-25629419521d/download/Non%20Regulated%20Lead%20Samples.csv",
+  show_col_types = FALSE,
+  skip = 1
+  
+)
 
 
 
 #### Save data ####
 # [...UPDATE THIS...]
 # change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+write_csv(raw_lead_data, "inputs/data/raw_data.csv") 
 
          
